be8740bde73e2cdd95986c11055f6d9d8f305c47
Sarwar Ahammed <110556391+sarwar1ahammed@users.noreply.github.com>
2022-10-28 19:59:01 +0530

    Create Ques08.c

---
+++ Ques08.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/* 
+    A program to find the second smallest number in an array.
+    Take array values from the user.
+*/
+int main()
+{
+    int n, i, j, swap;
+    
+    printf("Enter array range : ");
+    scanf("%d", &n);
+    int a[n];
+    printf("Enter %d numbers of value to sort elements : \n", n);
+    for (i=0; i<=n-1; i++)
+        scanf("%d", &a[i]);
+
+    for (i=0; i<n-1; i++)
+    {
+        for (j=i+1; j<=n-1; j++)
+        {
+            if (a[i] > a[j])
+            {
+                swap = a[i];
+                a[i] = a[j];
+                a[j] = swap;
+            }
+        }
+    }
+
+    printf("Second smallest number in the array is : %d ", a[n-(n-1)]);
+
+        
+    printf("\n");
+    return 0;
+}
